17fdd5b515879c95cdc5c9999a503cd856a00543
Rohit Patidar <rohitpatidar@Rohits-MacBook-Air.local>
2024-09-20 03:54:52 +0530

    utex

--- video_2_racecondition/race_condition.cpp
+++ video_2_racecondition/race_condition.cpp
@@ -2,15 +2,16 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<pthread.h>
+//how to create two threads
+int mails=0;
 void * routine (void *)
     {
-        static int i=0;
-        printf("Test for threads %d\n",i);
-        i=i+1;
-        sleep(3);
-        printf("End Threds %d\n",i);
-        i=i+1;
-        return NULL;
+        for(int i=0;i<50000;i++)
+           {
+                mails++;   //racecondition
+                           //read increment write
+           };
+           return NULL;
     };
      
      int main()
@@ -20,5 +21,6 @@ void * routine (void *)
            pthread_create(&t1,NULL,&routine,NULL);
            pthread_join(t1,NULL);
            pthread_join(t2,NULL);
+           printf("mails :%d",mails);
            return 0;
      };
